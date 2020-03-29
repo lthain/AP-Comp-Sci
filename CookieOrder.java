051d8ccd8885dd4e4eb09b6ec34b1aa8964c536c
lthain <42009917+lthain@users.noreply.github.com>
2020-03-28 22:51:49 -0400

    Goes with order app

---
+++ CookieOrder.java
@@ -0,0 +1,19 @@
+
+public class CookieOrder {
+	private String variety;
+	private int numBoxes;
+	
+	public String getVariety() {
+		return variety;
+	}
+	
+	public int getNumBoxes() {
+		return numBoxes;
+	}
+	
+	public CookieOrder(String variety, int numBoxes) {
+		this.variety = variety;
+		this.numBoxes = numBoxes;
+	}
+
+}
